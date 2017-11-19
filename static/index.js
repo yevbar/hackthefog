b01966606ff36cab40c210e3e4c00f0ac343d042
Yevgeniy Y Barkalov <yevgeniybarkalov@yahoo.com>
2017-11-19 10:48:58 -0800

    Moved javascript from html to static file

--- static/index.js
+++ static/index.js
@@ -1,3 +1,12 @@
-function helloword() {
-  return "Hello World";
+var myIndex = 0
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("info-tab");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
 }
