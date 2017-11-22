e4b790f07afc33311c6fa051d2ae9d3e97cdf5d9
Savagecoder77 <savagecoder77@gmail.com>
2017-11-21 21:16:24 -0800

    animation change

--- static/index.js
+++ static/index.js
@@ -3,3 +3,8 @@ var myIndex = 0
 function carousel(input) {
   $('html, body').animate({scrollTop: $(input).offset().top}, 800)
 }
+$(function() {
+	$("#visible").click(function() {
+		$('#invisible').toggleClass("show");
+	});
+});
