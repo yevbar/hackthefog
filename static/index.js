d5a8f417e0b9dc0939f1df1d30b4329c9b05e628
Alex Ruiz <ruiz.alex0302@gmail.com>
2017-11-27 00:09:27 -0800

    Added smoother animations

--- static/index.js
+++ static/index.js
@@ -6,10 +6,22 @@ function carousel(input) {
 $(function() {
   //Animate showing answers on click
   $('.q').click(function(e) {
-    $(e.target).next('.a').animate({
-      opacity: "toggle"
-    }).css({
-      display: "inline-block"
-    });
+    var a = $(e.target).next('.a');
+
+    if (a.css('visibility') == 'hidden'){
+      //Animate it to bounce in if it isn't visible
+      a.css({
+        visibility: "visible"
+      }).toggleClass("animated bounceInRight");
+      //If it has bounceOut class, remove it
+      if (a.hasClass('flipOutX')) a.toggleClass('flipOutX');
+    } else {
+      a.toggleClass("bounceInRight flipOutX");
+      $('#yourElement')
+        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+      , function(){
+        a.css({visibility: "hidden"});
+      });
+    }
   });
 });
