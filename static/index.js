501e082e62498e37bc5fd46d92278505fbd4781d
Alex Ruiz <ruiz.alex0302@gmail.com>
2017-11-29 10:51:27 -0800

    Work on js to make pop-out thing

--- static/index.js
+++ static/index.js
@@ -4,28 +4,48 @@ function carousel(input) {
   $('html, body').animate({scrollTop: $(input).offset().top}, 800)
 }
 $(function() {
+
+  //Variable for the innerHTML of the .a elements
+  var answers = [];
+  document.getElementsByClassName('a').forEach(function(item, index){
+    answers[index] = item;
+    document.getElementsByClassName('a')[index] = "";
+  });
+
   //Animate showing answers on click
   $('.q').click(function(e) {
     var a = $(e.target).next('.a');
 
-    if (a.css('visibility') == 'hidden'){
-      //Animate it to bounce in if it isn't visible
-      a.css({
-        visibility: "visible"
-      }).addClass("animated flipInX")
-        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
-        , function(){
-            a.removeClass('animated flipInX');
-      });
+    if(a.css("width") == "0px"){
+      a.animate(
+        {
+          width:"48%"
+        },
+        {
+          complete: function(){
+            a.css("display", "inline-block");
+            var index = $('.q').toArray().forEach(function(item, index){
+              if ($('.q').toArray()[index] === e.target) return index;
+            });
+            a.HTML(answers[index]);
+          }
+        }
+      );
     } else {
-      a.addClass("animated flipOutX")
-        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
-        , function(){
-          a.css({visibility: "hidden"})
-            .removeClass('animated flipOutX');
-      });
+      a.animate(
+        {
+          width:"0%"
+        },
+        {
+          start: function(){
+            a.HTML("");
+            a.css("display", "none");
+          }
+        }
+      );
     }
-  });
+  }
+  );
 
   //Animate questions on hover
   $('.q').hover(function(e){
