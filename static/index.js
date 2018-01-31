508163192a5979b23b1175c89ec0615586fdacb8
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-01-30 21:32:22 -0800

    Create sponsor dropdown

--- static/index.js
+++ static/index.js
@@ -1,48 +1,7 @@
-var myIndex = 0
-
-function carousel(input) {
-  $('html, body').animate({scrollTop: $(input).offset().top}, 800)
+function showSponsors() {
+  document.getElementById("sponsors").style.height = "100%";
 }
-$(function() {
 
-  //Animate showing answers on click
-  $('.q').click(function(e) {
-    $(e.target).toggleClass("animated bounce");
-
-    var a = $(e.target).next('.a');
-
-    if(a.css("width") == "0px"){
-      a.animate(
-        {
-          width:"43%",
-          height:"100%"
-        },
-        {
-          complete: function(){
-            a.animate({"font-size": "1em"});
-          },
-          start: function(){
-            a.css("display", "inline-block")
-          }
-        }
-      );
-    } else {
-      a.animate(
-        {
-          width:"0px",
-          height:"0px"
-        },
-        {
-          start: function(){
-            a.animate({"font-size": "0px"});
-          },
-          complete: function(){
-            a.css("display", "none");
-          }
-        }
-      );
-    }
-  }
-  );
-
-});
+function hideSponsors() {
+  document.getElementById("sponsors").style.height = "0%";
+}
