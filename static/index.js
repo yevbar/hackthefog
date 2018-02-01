9da313aaf93795378e996cdb43e0230c5c6faa3b
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-02-01 11:34:56 -0800

    Finish sponsor and schedule buttons

--- static/index.js
+++ static/index.js
@@ -1,15 +1,19 @@
 function showSponsors() {
+  document.getElementById("sponsors").style.zIndex = "1";
   document.getElementById("sponsors").style.height = "100%";
 }
 
 function hideSponsors() {
   document.getElementById("sponsors").style.height = "0%";
+  document.getElementById("sponsors").style.zIndex = "0";
 }
 
 function showSchedule() {
   document.getElementById("schedule").style.height = "100%";
+  document.getElementById("schedule").style.zIndex = "1";
 }
 
 function hideSchedule() {
   document.getElementById("schedule").style.height = "0%";
+  document.getElementById("schedule").style.zIndex = "0";
 }
